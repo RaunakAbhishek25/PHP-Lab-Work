4d95201dd308078d7275ef1349c962765ffa169b
RaunakAbhishek25 <abhishekraunak793@gmail.com>
2026-06-11 11:48:45 +0530

    Add files via upload

---
+++ MaxiandMini.php
@@ -0,0 +1,16 @@
+<?php
+$num1 = 25;
+$num2 = 50;
+$num3 = 10;
+
+$max = max($num1, $num2, $num3);
+
+$min = min($num1, $num2, $num3);
+
+echo "Number 1 = $num1 <br>";
+echo "Number 2 = $num2 <br>";
+echo "Number 3 = $num3 <br><br>";
+
+echo "Maximum Number = " . $max . "<br>";
+echo "Minimum Number = " . $min;
+?>
