484e43c96f2029f9d50185e63272004d41aa451b
RaunakAbhishek25 <abhishekraunak793@gmail.com>
2026-06-15 09:46:34 +0530

    Add files via upload

---
+++ collegeInfo.php
@@ -0,0 +1,10 @@
+<?php
+function collegeInfo()
+{
+    echo "College Name: Marwadi Univeersity College<br>";
+    echo "Course Name: Bachelor of Computer Applications (BCA)<br>";
+    echo "Academic Year: 2024-2027";
+}
+
+collegeInfo();
+?>
