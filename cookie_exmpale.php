f80531f845950dbf07df0ef739493b970c5505d9
RaunakAbhishek25 <abhishekraunak793@gmail.com>
2026-08-05 08:20:25 +0530

    Add files via upload

---
+++ cookie_exmpale.php
@@ -0,0 +1,29 @@
+<?php
+
+// function enciypt_cookie($user_value)
+//{
+//   $user_value = $user_value . "434873847";
+//   $data = setcookie("user", "$user_value", time() +300);
+//   return $data;
+//}
+
+// cookie is a persistant storage  but with a expiry timeline.
+// for performance tracing of website
+// store information related to user search data 
+// Data stored in cookie converted to table.
+
+// creating cookie
+// setcookie("user", "Raunak", time() + 300);
+
+// editing cookie
+// setcookie("user", "developer", time() + 300);
+
+// deleting cookie
+
+setcookie("user" , "Raunak" , time()+300);
+
+
+
+
+
+?>
